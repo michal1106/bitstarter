bb104c6779043b840406ed8700b71a715a9f587d
Ubuntu <ubuntu@ip-172-31-32-209.us-west-2.compute.internal>
2013-07-25 21:11:53 +0000

    add utf8

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var text = fs.readFileSync('index.html')
+  var text = fs.readFileSync('index.html', 'utf8')
   response.send(text);
 });
 
