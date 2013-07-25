9e65eaf5684a1e740336ef7565ef5c687e4acb93
Ubuntu <ubuntu@ip-172-31-32-209.us-west-2.compute.internal>
2013-07-25 21:02:29 +0000

    try fs.readFileSync

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var text = fs.readFileSync('index.html')
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
